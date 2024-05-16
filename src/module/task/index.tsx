a3704804bf4b7e1e80b97d86cfd4bc9d4b4c5ec6
mohamad ali <M.Roudkhane@asanpardakht.ir>
2024-05-16 15:07:05 +0330

    feat(todoLogics): implement all logic in document

--- src/module/task/index.tsx
+++ src/module/task/index.tsx
@@ -1,14 +1,37 @@
 import {observer} from "mobx-react";
 import useStores from "../../hooks/useStores";
 import {RecursiveTask} from "./components";
+import tasks from "../../store/tasks";
 
 export const TaskModule = observer(() => {
     const {TasksStore} = useStores();
+    const handleTitleChange = (id: string, value: string) => {
+        tasks.changeTaskTitle({taskId: id, newTitle: value});
+    };
+    const handleOrderChange = (id: string, parentId: string | null, position: 'up' | 'down') => {
+        tasks.changeOrder({id, position});
+    };
+    const handleAddNewNestedTask = (parentId: string) => {
+        tasks.addNestedTask({parentTaskId: parentId});
+    };
+    const handleDeleteTask = (id: string) => {
+        tasks.deleteTask({id});
+    };
+    const handleToggleExpanded = (id: string) => {
+        tasks.toggleExpanded({id});
+    };
+
 
     return (
         <div>
             {TasksStore?.tasks?.map(item => {
-                return <RecursiveTask task={item} key={item.id}/>
+                return <RecursiveTask task={item}
+                                      key={item.id}
+                                      handleToggleExpanded={handleToggleExpanded}
+                                      handleOrderChange={handleOrderChange}
+                                      deleteTask={handleDeleteTask}
+                                      addNestedTask={handleAddNewNestedTask}
+                                      onChangeTaskTitle={handleTitleChange}/>
             })}
         </div>
     );
